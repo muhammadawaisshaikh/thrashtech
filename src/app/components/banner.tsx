db7bbd69ab928099a7391d72f8ad0d0526eecc2d
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-14 13:57:50 +0500

    banner and header fix

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -9,14 +9,14 @@ export default function Banner() {
                     <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                         <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                             AI is embedded in everything we do.
-                            <a href="#" className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+                            <a href="#" className="font-semibold text-dark-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
                         </div>
                     </div>
                     <div className="text-center">
                         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Collaborator to enrich your Innovations</h1>
                         <p className="mt-6 text-lg leading-8 text-gray-600">A worldwide digital collaborator that assists businesses in undergoing transformation, innovating new products, and expanding their teams.</p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
-                            <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+                            <a href="#" className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black-600">Get started</a>
                             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
                         </div>
                     </div>
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,8 +4,8 @@ export default function Header() {
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
-                        <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                        <span className="sr-only">Thrashtech</span>
+                        <img className="h-8 w-auto" src="https://i.ibb.co/7JNwXdC/t.png" alt="" />
                     </a>
                 </div>
                 <div className="flex lg:hidden">
