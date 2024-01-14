0497e826a27613c3a6f9b046ee361588089a0768
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-15 00:34:30 +0500

    team text added

--- src/app/components/teamSection.tsx
+++ src/app/components/teamSection.tsx
@@ -15,7 +15,7 @@ export default function TeamSection() {
             <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
                 <div className="max-w-2xl">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our Experts!</h2>
-                    <p className="mt-6 text-lg leading-8 text-gray-600">Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae elementum enim vitae ullamcorper suspendisse.</p>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">Global team of creators and innovators! We prioritize consistently delivering innovation.</p>
                 </div>
                 <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
                     {
