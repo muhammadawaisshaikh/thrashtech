7ea83dc5a7f81cac8d8e63acf62ade5319c921c0
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-15 00:29:30 +0500

    team shade added

--- src/app/components/teamSection.tsx
+++ src/app/components/teamSection.tsx
@@ -1,3 +1,5 @@
+import Shade from "./shade";
+
 export default function TeamSection() {
 
     const team = [
@@ -8,7 +10,8 @@ export default function TeamSection() {
     ]
 
     return (
-        <div className="bg-white py-24 sm:py-32">
+        <div className="bg-white py-24 sm:py-32 relative isolate">
+            <Shade position="top" />
             <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
                 <div className="max-w-2xl">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our Experts!</h2>
