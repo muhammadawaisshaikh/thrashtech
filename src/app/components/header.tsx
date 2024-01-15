20dd6018880a4842d813e6a6546f4fd8151a8d4a
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-15 18:24:00 +0500

    fix stroke-width

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -41,7 +41,7 @@ export default function Header() {
                         </a>
                         <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
                             <span className="sr-only">Close menu</span>
-                            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         </button>
