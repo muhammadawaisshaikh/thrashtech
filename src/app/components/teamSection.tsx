6c90712a1b7fe2f100737c056a698703519c6081
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-15 18:45:50 +0500

    Missing "key" prop for element in iterator

--- src/app/components/teamSection.tsx
+++ src/app/components/teamSection.tsx
@@ -3,10 +3,10 @@ import Shade from "./shade";
 export default function TeamSection() {
 
     const team = [
-        { name: "Ilyas Ahmed", designation: "Chief Executive Officer", image: "https://media.licdn.com/dms/image/D4D03AQGXAYn7wEtnSQ/profile-displayphoto-shrink_800_800/0/1689973889622?e=1710979200&v=beta&t=mr3oyLRlPp23D0C5clINwlF539uvTxZA21wXwYZKyuA" },
-        { name: "Muhammad Awais", designation: "Chief Technology Officer", image: "https://avatars.githubusercontent.com/u/24633059?v=4" },
-        { name: "Muhammad Idrees", designation: "Development Manager", image: "https://media.licdn.com/dms/image/C5103AQH5BcbpI7WHaw/profile-displayphoto-shrink_800_800/0/1544538913257?e=1710979200&v=beta&t=wW-VT26v3UY8LfIcMzwpIOS6NEAamjhrnDtpNPOo89U" },
-        { name: "Muhammad Idrees", designation: "Team Lead Web Technology", image: "https://media.licdn.com/dms/image/D4D03AQHLVcLIK9XJHg/profile-displayphoto-shrink_800_800/0/1696415837192?e=1710979200&v=beta&t=HJ0nZwWR2AJC_9ZyX-9bHTeOoq6Jt-ddJwgfPpFSgDY" }
+        { id: 1, name: "Ilyas Ahmed", designation: "Chief Executive Officer", image: "https://media.licdn.com/dms/image/D4D03AQGXAYn7wEtnSQ/profile-displayphoto-shrink_800_800/0/1689973889622?e=1710979200&v=beta&t=mr3oyLRlPp23D0C5clINwlF539uvTxZA21wXwYZKyuA" },
+        { id: 2, name: "Muhammad Awais", designation: "Chief Technology Officer", image: "https://avatars.githubusercontent.com/u/24633059?v=4" },
+        { id: 3, name: "Muhammad Idrees", designation: "Development Manager", image: "https://media.licdn.com/dms/image/C5103AQH5BcbpI7WHaw/profile-displayphoto-shrink_800_800/0/1544538913257?e=1710979200&v=beta&t=wW-VT26v3UY8LfIcMzwpIOS6NEAamjhrnDtpNPOo89U" },
+        { id: 4, name: "Muhammad Idrees", designation: "Team Lead Web Technology", image: "https://media.licdn.com/dms/image/D4D03AQHLVcLIK9XJHg/profile-displayphoto-shrink_800_800/0/1696415837192?e=1710979200&v=beta&t=HJ0nZwWR2AJC_9ZyX-9bHTeOoq6Jt-ddJwgfPpFSgDY" }
     ]
 
     return (
@@ -21,7 +21,7 @@ export default function TeamSection() {
                     {
                         team?.map(member => {
                             return (
-                                <li>
+                                <li key={member.id}>
                                     <div className="flex items-center gap-x-6">
                                         {/* <img className="h-16 w-16 rounded-full" src={member.image} alt="" /> */}
                                         <div>
