ca3d2002fabe487abc868594799ace7dc157937c
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-16 19:52:16 +0500

    banner fix

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,16 +1,32 @@
 import Shade from "./shade";
 
+const chips = [
+    { id: 1, typo: "AI is embedded in everything we do." },
+    // { id: 2, typo: "We add a secret sauce in our deliverables." },
+    // { id: 3, typo: "Decisions with Data, Results with Intelligence." },
+    // { id: 4, typo: "Smarter Healthcare, Streamlined Business." },
+    // { id: 5, typo: "Beyond Code from Factories to Finances" },
+]
+
 export default function Banner() {
     return (
         <div className="bg-white">
             <div className="relative isolate px-6 lg:px-8">
                 <Shade position="top" />
                 <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-30">
-                    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                        <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                            AI is embedded in everything we do.
-                            <a href="#" className="font-semibold text-dark-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
-                        </div>
+                    <div className="scroll-container">
+                        {
+                            chips.map((chip) => {
+                                return (
+                                    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                                        <div key={chip.id} className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                                            {chip.typo}
+                                            <a href="#" className="font-semibold text-dark-600"><span className="absolute inset-0" aria-hidden="true"></span></a>
+                                        </div>
+                                    </div>
+                                );
+                            })
+                        }
                     </div>
                     <div className="text-center">
                         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Collaborator to enrich your Innovations</h1>
