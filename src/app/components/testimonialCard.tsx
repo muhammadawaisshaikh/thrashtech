a1c67d7fc9e5247b5507da1e07a09cfb4bd9afbd
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-19 00:13:19 +0500

    added key prop in testimonial to fix vercel deployment pipeline

--- src/app/components/testimonialCard.tsx
+++ src/app/components/testimonialCard.tsx
@@ -6,7 +6,7 @@ interface TestimonialCardProps {
 
 const TestimonialCard: React.FC<TestimonialCardProps> = ({ testimonial }) => {
     return (
-        <div className="relative isolate overflow-hidden bg-white px-6 lg:px-8">
+        <div className="relative isolate overflow-hidden bg-white px-6 lg:px-8" key={testimonial.id}>
             <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
             <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
 
