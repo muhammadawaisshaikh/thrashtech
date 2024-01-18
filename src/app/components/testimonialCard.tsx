8f2686d8662076f4b0c2f6f594290008bbdfbd8f
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-19 00:30:04 +0500

    changed the images cdn to imgbb by hosting as a cdn also added Image component from next to optimize imah=ges as LCP

--- src/app/components/testimonialCard.tsx
+++ src/app/components/testimonialCard.tsx
@@ -12,7 +12,7 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ testimonial }) => {
             <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
 
             <div className="mx-auto max-w-2xl lg:max-w-6xl">
-                <Image className="mx-auto w-20 h-20 rounded-full" src={testimonial.companyLogo} alt="" />
+                <Image className="mx-auto w-20 h-20 rounded-full" src={testimonial.companyLogo} alt="companyLogo" width={500} height={500} />
                 <figure className="mt-10">
                     <blockquote className="text-center text-xl font-normal leading-8 text-gray-900 sm:text-2xl sm:leading-9">
                         <p>
@@ -23,7 +23,9 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ testimonial }) => {
                         <Image
                             className="mx-auto h-10 w-10 rounded-full"
                             src={testimonial.clientImage}
-                            alt=""
+                            alt="clientImage"
+                            width={500} 
+                            height={500}
                         />
                         <div className="mt-4 flex items-center justify-center space-x-3 text-base">
                             <div className="font-semibold text-gray-900">{testimonial.companyName}</div>
--- src/utils/mock-data/testimonials.ts
+++ src/utils/mock-data/testimonials.ts
@@ -3,41 +3,41 @@ import { Testimonial } from "@/app/types/testimonial";
 export const testimonials: Testimonial[] = [
     {
         id: 1,
-        companyLogo: "https://media.licdn.com/dms/image/D4D0BAQHEq6LmT6r-5A/company-logo_200_200/0/1695848135500/platformatic_logo?e=1713398400&v=beta&t=WQVEBCYingKBqjHVJGvptuKfOVN-k5KaNjdVBMsk_JE",
+        companyLogo: "https://i.ibb.co/XpBKDLq/platformatic-logo.jpg",
         description: "Thrashtech's unwavering enthusiasm and determination to explore and master emerging technologies are incredibly invigorating. Thrashtech seamlessly blends profound expertise with a fervent commitment to guiding and mentoring others, steering developers towards their journey of advancement. Thrashtech's active engagement with diverse communities positions them as an outstanding team member, and their dedication to open source initiatives and innovation adds significant value to any organization.",
-        clientImage: "https://media.licdn.com/dms/image/D5603AQH3vnUf1b0Ecw/profile-displayphoto-shrink_800_800/0/1669749359325?e=1710979200&v=beta&t=3D57U3ZqYDBkWubZ2eWkPqstgp3phoUi281x5TjZXKg",
+        clientImage: "https://i.ibb.co/wdLqbvr/1669749359325.jpg",
         clientDesignation: "Co-Founder and CEO @Platformatic",
         companyName: "Luca Maraschi"
     },
     {
         id: 2,
-        companyLogo: "https://media.licdn.com/dms/image/C4D0BAQFyYsh81MQUJQ/company-logo_200_200/0/1633026096421/giraphix_creative_logo?e=1713398400&v=beta&t=RXetT7VNS3AMkgr1v8isD5JfZX_mBjNNcUzIPIveHhA",
+        companyLogo: "https://i.ibb.co/3rQr1sQ/giraphix-creative-logo.jpg",
         description: "The Thrashtech team is comprised of exceptional full-stack developers. Throughout all of our projects together over the last few years, they have demonstrated excellent communication, reliability, and a strong work ethic. Thrashtech pays great attention to detail on the front end and exhibits exceptional problem-solving abilities on the backend. They are undeniably crucial to any organization's success.",
-        clientImage: "https://media.licdn.com/dms/image/D5603AQGE6aAkaeA9kQ/profile-displayphoto-shrink_200_200/0/1690248948029?e=1710979200&v=beta&t=ILGhG98km3q7629kCyvnkRUHv3DmS98cA_000oxLnOY",
+        clientImage: "https://i.ibb.co/GQFNxbC/1690248948029.jpg",
         clientDesignation: "CEO @Giraphix Creative",
         companyName: "Lucas Greg Milano"
     },
     {
         id: 3,
-        companyLogo: "https://media.licdn.com/dms/image/C4D0BAQFK8g-6A934tQ/company-logo_200_200/0/1630531918340/firekamp_logo?e=1713398400&v=beta&t=EvYGRIJsXTDeUntkC22s1dBZJ-YtMwrN-GrV7dIXXiU",
+        companyLogo: "https://i.ibb.co/G2msvnx/firekamp-logo.jpg",
         description: "Thrashtech not only delivered a high-quality product but also exhibited great communication, reliability, and a strong work ethic. Their commitment to excellence and problem-solving abilities significantly contributed to the success of our project. I highly recommend Thrashtech for any development endeavors and look forward to future collaborations.",
-        clientImage: "https://media.licdn.com/dms/image/D4E03AQGZlzl_hOML3w/profile-displayphoto-shrink_200_200/0/1666644423772?e=1710979200&v=beta&t=-a3c4KHySUeundmfZbr8FxWHyzMf-UosYGaUPjFk94Q",
+        clientImage: "https://i.ibb.co/7pp2KCR/1666644423772.jpg",
         clientDesignation: "Founder @FireKamp",
         companyName: "Kamran Pirwani"
     },
     {
         id: 4,
-        companyLogo: "https://myareanetwork-photos.s3.amazonaws.com/bizlist_photos/t/195507_1583354511.png",
+        companyLogo: "https://i.ibb.co/fqxdtcd/195507-1583354511.png",
         description: "I had the pleasure of working with Thrashtech on the development of our Event Management App - Unation. The team's proficiency in app development was truly remarkable. Thrashtech demonstrated a keen understanding of our project requirements and showcased outstanding technical skills throughout the collaboration.",
-        clientImage: "https://media.licdn.com/dms/image/C4D03AQEMLrqOE5yalA/profile-displayphoto-shrink_200_200/0/1517073477181?e=1710979200&v=beta&t=WIJsHi4b3Wml6TItrTZ428h5pdg8DNjaoqqw0gbyLJs",
+        clientImage: "https://i.ibb.co/DYpxXk5/1517073477181.jpg",
         clientDesignation: "Co-Founder and COO @UNATION",
         companyName: "George Beardsley"
     },
     {
         id: 5,
-        companyLogo: "https://media.licdn.com/dms/image/C560BAQEzLPfBXqhV-A/company-logo_200_200/0/1679521599116/fletcher_building_logo?e=1713398400&v=beta&t=dHyn_NHtjl5t-Z0DPtVgSIohvs8VmFc7Was4q0um78s",
+        companyLogo: "https://i.ibb.co/ChdB8kX/fletcher-building-logo.jpg",
         description: "Thrashtech played a crucial role in the development of our renowned Ecommerce Web App for Fletcher Building. Their expertise and dedication were evident throughout the project. With operations spanning manufacturing, distribution, retail, and construction in NZ, Australia, and the South Pacific, Fletcher Building required a robust and tailored solution.",
-        clientImage: "https://media.licdn.com/dms/image/D5603AQFyimiUHFhL4g/profile-displayphoto-shrink_200_200/0/1688600574107?e=1710979200&v=beta&t=VL8qUJh1Z-bo01QadRngIPx4wHKBV-e79rMgvCafK50",
+        clientImage: "https://i.ibb.co/kMJTyq7/1688600574107.jpg",
         clientDesignation: "GM of Ecommerce @Fletcher Building",
         companyName: "Mark Phillips"
     }
