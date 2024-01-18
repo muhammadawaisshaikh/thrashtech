6f80f4fca0421dfc607c45d94c49f3bc9b864bab
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-19 00:42:33 +0500

    Open Graph tags for social media sharing

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -30,7 +30,7 @@ export default function Banner() {
                     </div>
                     <div className="text-center">
                         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Collaborator to enrich your Innovations</h1>
-                        <p className="mt-6 text-lg leading-8 text-gray-600">A worldwide digital collaborator that assists businesses in undergoing transformation, innovating new products, and expanding their teams.</p>
+                        <p className="mt-6 text-lg leading-8 text-gray-600">A global digital collaborator (Web, Mobile & AI) dedicated to guiding businesses through transformative journeys, fostering innovation in product development, and supporting team expansion initiatives.</p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
                             <a href="#" className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black-600">Get started</a>
                             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
